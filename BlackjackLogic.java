157cd1e79450cc1626884ef1dd1c56cc77665199
unknown <Parrishbritt1@gmail.com>
2022-09-14 21:51:19 -0400

    Made Player class, hit method, list of cards for their hand, and value of hand calculations

---
+++ BlackjackLogic.java
@@ -0,0 +1,10 @@
+package Blackjack;
+
+import Blackjack.Deck;
+
+
+public class BlackjackLogic {
+    public BlackjackLogic() {
+        Deck deck = new Deck();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,54 @@
+package Blackjack;
+
+import Blackjack.CardValue;
+import Blackjack.Card;
+import java.util.ArrayList;
+
+
+public class Player {
+    private ArrayList<Card> hand;
+    private int valueOfHand;
+    private String name;
+
+    /**
+     * Player Constructor
+     * @param name Name of the player
+     */
+    public Player(String name) {
+        this.hand = new ArrayList<Card>();
+        this.name = name;
+        this.valueOfHand = 0;
+    }
+
+    public int valueOfHand() {
+        return this.valueOfHand;
+    }
+
+    /**
+     * Player hits (adds card to hand and updates value of hand)
+     * @param card Card that will be added to hand
+     */
+    public void hit(Card card) {
+        this.hand.add(card);
+        updateValueOfHand();
+    }
+
+    /**
+     * Private helper method
+     * Gets the value of player's hand
+     * @return value of hand
+     */
+    private int updateValueOfHand() {
+        for (Card c : hand) {
+            if (c.getValue() == CardValue.ACE) {
+                if (this.valueOfHand + c.getValue().getValue() > 21) {
+                    this.valueOfHand += 1;
+                    continue;
+                }
+            } else {
+                this.valueOfHand += c.getValue().getValue();
+            }
+        }
+        return this.valueOfHand;
+    }
+}
